7bb1d71fecd61bca5eeb999d78b01fae469f209d
Didinzz <didinzakaria123@gmail.com>
2025-09-17 09:26:24 +0800

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 
 export default function Home() {
+  redirect("/login");
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
